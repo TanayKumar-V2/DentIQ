d8e7d840e6a7d95a2e6e282beb4c6c3d8d2031ce
TanayKumar-V2 <tanayk726@gmail.com>
2026-02-24 15:05:37 +0530

    Added landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,21 @@
-import { Button } from "@/components/ui/button";
-import { SignUpButton } from "@clerk/nextjs";
+import CTA from "@/components/Landing/CTA";
+import Footer from "@/components/Landing/Footer";
+import Header from "@/components/Landing/Header";
+import Hero from "@/components/Landing/Hero";
+import HowItWorks from "@/components/Landing/HowItWorks";
+import PricingSection from "@/components/Landing/PricingSection";
+import WhatToAsk from "@/components/Landing/WhatToAsk";
 
 export default function Home(){
   return(
-   <div>
-     <h1>
-      Home Page
-    </h1>
-    <SignUpButton mode="modal"></SignUpButton>
+   <div className="min-h-screen bg-background">
+    <Header/>
+    <Hero/>
+    <HowItWorks/>
+    <WhatToAsk/>
+    <PricingSection/>
+    <CTA/>
+    <Footer/>
    </div>
   )
 }
